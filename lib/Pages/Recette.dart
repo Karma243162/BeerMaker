f5b6bcbcb698612281c37fc0783bcad83a307968
Karma243162 <97706454+Karma243162@users.noreply.github.com>
2023-10-08 13:31:31 +0200

    Phase de preparation

--- lib/Pages/Recette.dart
+++ lib/Pages/Recette.dart
@@ -11,13 +11,45 @@ class _RecetteBeerState extends State<RecetteBeer> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Recette Bière',
+          'Etape de Fabrication',
           style: TextStyle(color: Color.fromARGB(255, 255, 193, 7)),
         ),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-        
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              alignment: Alignment.center,
+              child: Text(
+                '0. Phase Préparation',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Vous avez besoin du matériel suivant pour realiser votre bière :',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            SizedBox(height: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('-Une damme-jeanne'),
+                Text('-Un barboteur'),
+                Text('-Un thermomètre'),
+                Text('-un Tuyau d extraction'),
+                Text('-plusieurs casseroles'),
+                Text('-une passoire à maille fines'),
+              ],
+            ),
+          ],
         ),
       ),
     );
